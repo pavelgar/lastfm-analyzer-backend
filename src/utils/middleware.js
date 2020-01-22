9f16ebeb9eb6b1fda741663a426588b746d77590
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2020-01-22 15:51:48 +0200

    Move middleware out and add mongo connection

---
+++ src/utils/middleware.js
@@ -0,0 +1,27 @@
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" })
+}
+
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return res.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message })
+  } else if (error.name === "JsonWebTokenError") {
+    return res.status(401).json({
+      error: "invalid token"
+    })
+  }
+  console.log(error.message)
+  next(error)
+}
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    req.token = authorization.substring(7)
+  }
+  next()
+}
+
+module.exports = { unknownEndpoint, errorHandler, tokenExtractor }
