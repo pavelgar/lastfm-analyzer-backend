0b375e8cec7aca1c5fcf2d4f3569db039f44cc19
pavelgar <pavel.garmuyev@gmail.com>
2020-01-27 18:40:22 +0200

    Don't use reduce

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -11,12 +11,14 @@ const JWT_SECRET = process.env.JWT_SECRET
 
 var router = Router()
 
-const getSignature = (params, secret) =>
-  md5(
-    Object.keys(params)
-      .sort()
-      .reduce((signature, key) => signature + key + params[key], "") + secret
-  )
+const getSignature = (params, secret) => {
+  const signature = Object.entries(params)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .flat()
+    .concat(secret)
+    .join("")
+  return md5(signature)
+}
 
 router.post("/", async (req, res) => {
   const payload = {
@@ -28,24 +30,25 @@ router.post("/", async (req, res) => {
   const api_sig = getSignature(payload, LASTFM_API_SECRET)
 
   try {
-    // Get session
     const session = await axios.get(LASTFM_API_ROOT, {
       params: { ...payload, format: "json", api_sig }
     })
     const { key, name } = session.data.session
     const token = jwt.sign(key, JWT_SECRET)
 
-    const user = await User.findOneAndUpdate(
-      { name },
-      { $set: { name } },
-      { upsert: true, new: true }
-    )
+    let user = await User.findOne({ name })
+    if (!user) {
+      user = await User.create({ name })
+    }
 
-    // Return signed token and username
     res.status(200).send({ token, name, id: user._id })
   } catch (error) {
-    console.log(error.response.data)
-    res.status(403).end()
+    if (error.response) {
+      const { status, data } = error.response
+      res.status(status).send(data)
+    } else {
+      res.status(403).end()
+    }
   }
 })
 
